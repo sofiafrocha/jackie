7e0bfd79347b024bf64f42bd20bbb46433e11eda
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2017-03-26 18:35:11 +0100

    Add a file watcher and browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,30 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+
+var distPath = 'assets/dist';
+var sassPath = 'assets/scr/scss';
 
 gulp.task('sass', function () {
-  return gulp.src('assets/scr/scss/**/*.scss')
+  return gulp.src(`${sassPath}/**/*.scss`)
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('assets/dist/css'));
+    .pipe(gulp.dest(`${distPath}/css`))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: {
+            baseDir: './',
+        }
+
+        // use another server
+        // proxy: '',
+    });
+
+    gulp.watch(`${sassPath}/**/*.scss`, ['sass']);
+    gulp.watch('./*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['sass']);
